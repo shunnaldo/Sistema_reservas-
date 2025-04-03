ac103454c00a4a860efd8d79a97df5fccbce8de5
zRivasRicardo <z.ricardo.rivas@gmail.com>
2025-04-03 15:33:29 -0300

    formTerminado

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 </head>
 <body>
 
+
+
 <div id="navbar-container"></div> 
 
 <div class="form-container">
@@ -51,7 +53,6 @@
             <div class="form-group">
             <label for="horaInicio">Hora de inicio:</label>
                 <select id="horaInicio" name="horaInicio" required class="input-text">
-                    <option value="08:00">08:00</option>
                     <option value="09:00">09:00</option>
                     <option value="10:00">10:00</option>
                     <option value="11:00">11:00</option>
@@ -61,14 +62,12 @@
                     <option value="15:00">15:00</option>
                     <option value="16:00">16:00</option>
                     <option value="17:00">17:00</option>
-                    <option value="18:00">18:00</option>
                 </select>
 
             </div>
             <div class="form-group">
             <label for="horaFin">Hora de fin:</label>
                 <select id="horaFin" name="horaFin" required class="input-text">
-                    <option value="09:00">09:00</option>
                     <option value="10:00">10:00</option>
                     <option value="11:00">11:00</option>
                     <option value="12:00">12:00</option>
@@ -78,7 +77,6 @@
                     <option value="16:00">16:00</option>
                     <option value="17:00">17:00</option>
                     <option value="18:00">18:00</option>
-                    <option value="19:00">19:00</option>
                 </select>
             </div>
             <button type="submit">Reservar</button>
