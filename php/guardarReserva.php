2a9f5ae21bf4c6c39e8acd7c3ad74aeb9efd7840
shunnaldo <benjaminignacioparramolina@gmail.com>
2025-04-16 17:24:13 -0400

    Update guardarReserva.php

--- php/guardarReserva.php
+++ php/guardarReserva.php
@@ -1,6 +1,6 @@
 <?php
 include('conexion.php');
-include('enviar_correo.php'); // Asegúrate de incluir el archivo donde tienes la función para enviar el correo
+include('enviar_correo.php');
 
 $data = json_decode(file_get_contents('php://input'), true);
 
@@ -17,40 +17,42 @@ $cowork = $data['cowork'];
 $fecha = $data['fecha'];
 $hora_inicio = $data['horaInicio'];
 $hora_fin = $data['horaFin'];
-$cantidad_personas = $data['cantidadPersonas']; // Nuevo campo recibido
+$cantidad_personas = $data['cantidadPersonas']; 
 $numero_telefono = $data['telefono'];
+
+
 
 $rut_cliente_sin_dv = substr($rut, 0, 8);
 
-$sql_check_rut = "SELECT * FROM ctrtecnicos WHERE LEFT(ctrtec_id, 8) = ?";
+$sql_check_rut = "SELECT * FROM ctrtecnicos WHERE LEFT(ctrtec_id, 8) = ? OR ctrtec_rut = ?";
 $stmt_check = $conexion->prepare($sql_check_rut);
-$stmt_check->bind_param("s", $rut_cliente_sin_dv);
+$stmt_check->bind_param("ss", $rut_cliente_sin_dv, $rut);
 $stmt_check->execute();
 $result_check = $stmt_check->get_result();
 
 if ($result_check->num_rows == 0) {
-    echo json_encode([ 
-        'success' => false, 
+    echo json_encode([
+        'success' => false,
         'message' => '¡Lo sentimos! No cuentas con la Tarjeta Vive La Florida. Solicítala acercándote a Casa Emprender ubicada en Alonso Ercilla #1380, La Florida. De Lunes a Jueves de 09:00 a 17:45 hrs y Viernes de 09:00 a 16:30 hrs. Recuerda traer tu carnét de identidad y comprobante de domicilio. ¡Te esperamos!'
     ]);
     exit;
 }
 
-// Verificar si el usuario ya tiene una reserva en la misma fecha
-$sql_check_reserva = "SELECT * FROM Reservas WHERE rut = ?";
+$sql_check_reserva = "SELECT * FROM Reservas WHERE rut = ? AND estado ='pendiente' or 'lista'";
 $stmt_check_reserva = $conexion->prepare($sql_check_reserva);
 $stmt_check_reserva->bind_param("s", $rut);
 $stmt_check_reserva->execute();
 $result_check_reserva = $stmt_check_reserva->get_result();
 
 if ($result_check_reserva->num_rows > 0) {
-    echo json_encode(['success' => false, 'message' => 'Usted ya cuenta con una reserva agendada']);
+    echo json_encode(['success' => false, 'message' => ' Usted ya cuenta con una reserva agendada']);
     exit;
 }
 
 $sql = "SELECT * FROM Reservas 
         WHERE fecha = ? 
         AND cowork = ? 
+        AND check_asistencia != 0
         AND ((hora_inicio <= ? AND hora_fin > ?) 
         OR (hora_inicio < ? AND hora_fin >= ?))";
 
@@ -62,32 +64,21 @@ $result = $stmt->get_result();
 if ($result->num_rows > 0) {
     echo json_encode(['success' => false, 'message' => 'La hora seleccionada ya está ocupada en este cowork.']);
 } else {
-    $sql_insert = "INSERT INTO Reservas (rut, nombre_vecino, apellido_vecino, correo_vecino, fecha, hora_inicio, hora_fin, cowork, cantidad_personas, numero_telefono) 
+
+
+    $sql_insert = "INSERT INTO Reservas (rut, nombre_vecino, apellido_vecino, correo_vecino, fecha, hora_inicio, hora_fin, cowork, cantidad_personas,numero_telefono) 
                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     $stmt_insert = $conexion->prepare($sql_insert);
-    $stmt_insert->bind_param("ssssssssss", $rut, $nombre, $apellido, $correo, $fecha, $hora_inicio, $hora_fin, $cowork, $cantidad_personas, $numero_telefono);
+    $stmt_insert->bind_param("ssssssssis", $rut, $nombre, $apellido, $correo, $fecha, $hora_inicio, $hora_fin, $cowork, $cantidad_personas, $numero_telefono);
     $stmt_insert->execute();
-    if ($stmt_insert->affected_rows > 0) {
-        echo json_encode(['success' => true, 'message' => 'Reserva realizada con éxito.']);
-    } else {
-        echo json_encode(['success' => false, 'message' => 'Error al realizar la reserva.']);
+
+    if ($stmt_insert->affected_rows > 0) {  
+        echo json_encode(['success' => true, 'message' => 'Reserva realizada con éxito. Te hemos enviado un correo de confirmación.']);
+        // Enviar el correo después de enviar la respuesta
+        enviarCorreoConfirmacion($correo, $nombre, $apellido, $rut, $cowork, $fecha, $hora_inicio, $hora_fin);
     }
-
-
-    if ($stmt_insert->affected_rows > 0) {
-        // Obtener el ID de la reserva recién insertada
-        $idReserva = $stmt_insert->insert_id;
-        
-        // Llamar a la función para enviar el correo
-        $enviado = enviarCorreoConfirmacion($idReserva);  // Llama a la función de correo con el ID de reserva
-
-        if ($enviado) {
-            echo json_encode(['success' => true, 'message' => 'Reserva realizada con éxito y correo enviado.']);
-        } else {
-            echo json_encode(['success' => false, 'message' => 'Reserva realizada, pero hubo un problema al enviar el correo.']);
-        }
-    } else {
+     else {
         echo json_encode(['success' => false, 'message' => 'Error al realizar la reserva.']);
     }
 }
